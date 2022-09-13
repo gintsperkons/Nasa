3e3127d0715de34ec2052a3474ab811acc06a4ee
Gints Kristaps Pērkons <gintskristaps13@gmail.com>
2022-09-13 07:06:44 +0000

    pievienoju komentārus

--- naked.py
+++ naked.py
@@ -1,3 +1,4 @@
+#---importē nepieciešamās bibliotēkas.
 import requests
 import json
 import datetime
@@ -5,42 +6,56 @@ import time
 import yaml
 
 from datetime import datetime
+
+#---izprintē teksti kas norāda uz programmas darbības sākumu.
 print('Asteroid processing service')
 
+#---izprintē teksti kas norāda uz programmas nākamo darbību.
 # Initiating and reading config values
 print('Loading configuration from file')
 
-# 
+#---nepieciešamie dati lai varētu izveidot pieteikumu nasa api.
 nasa_api_key = "yx2KdCqkWLyZaduhByErZWRuizwr4kSZWV9Wyebc"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
+#--- izveido datumu ar ko veiks pierasījumu no šibrīža laika ko iegūst ar datetime bibliotēku.
 dt = datetime.now()
 request_date = str(dt.year) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)  
 print("Generated today's date: " + str(request_date))
 
-
+#---izprintē pierasijuma saiti un veic pieprasījumu ar requests bibliotēku.
 print("Request url: " + str(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key))
 r = requests.get(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key)
 
+#--- izprintē pieprasījuma statusa datus un saturu.
 print("Response status code: " + str(r.status_code))
 print("Response headers: " + str(r.headers))
 print("Response content: " + str(r.text))
 
+
+#---status 200 nozīmē veiksmīgu pieprasījuma izpildi ja nav veigsmīgs tad nav nepieciešams datus mēģināt apstrādāt.
 if r.status_code == 200:
 
+	#--- pārveido tekstu par json formātu lai varētu vieglāk apstrādāt.
 	json_data = json.loads(r.text)
 
+	###---izveido sarakstus kuros glabās datus.
 	ast_safe = []
 	ast_hazardous = []
 
+	###---ja nav elementu daudzums (element_count) tad arī elementu nebūs.
 	if 'element_count' in json_data:
+		#--- izprintē elementu daudzumu.
 		ast_count = int(json_data['element_count'])
 		print("Asteroid count today: " + str(ast_count))
 
+		#---ja elementu eksistē tos apstrādā.
 		if ast_count > 0:
 			for val in json_data['near_earth_objects'][request_date]:
+				#---katru elementu pārbauda vai datu ir atrodamu.
 				if 'name' and 'nasa_jpl_url' and 'estimated_diameter' and 'is_potentially_hazardous_asteroid' and 'close_approach_data' in val:
+					#--- mainīgos elementiem piešķir datus
 					tmp_ast_name = val['name']
 					tmp_ast_nasa_jpl_url = val['nasa_jpl_url']
 					if 'kilometers' in val['estimated_diameter']:
@@ -55,23 +70,26 @@ if r.status_code == 200:
 						tmp_ast_diam_max = -1
 
 					tmp_ast_hazardous = val['is_potentially_hazardous_asteroid']
-
+					#--- vai ir dati par to ka būs tuvu
 					if len(val['close_approach_data']) > 0:
+						#--- mainīgos elementiem piešķir datus
 						if 'epoch_date_close_approach' and 'relative_velocity' and 'miss_distance' in val['close_approach_data'][0]:
 							tmp_ast_close_appr_ts = int(val['close_approach_data'][0]['epoch_date_close_approach']/1000)
+							#--- parveido datumu no epoch timestamp uz datuma formātu
 							tmp_ast_close_appr_dt_utc = datetime.utcfromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
 							tmp_ast_close_appr_dt = datetime.fromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
-
+							#--- mainīgos elementiem piešķir datus ja ir dati
 							if 'kilometers_per_hour' in val['close_approach_data'][0]['relative_velocity']:
 								tmp_ast_speed = int(float(val['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']))
 							else:
 								tmp_ast_speed = -1
-
+							#--- mainīgos elementiem piešķir datus ja ir dati
 							if 'kilometers' in val['close_approach_data'][0]['miss_distance']:
 								tmp_ast_miss_dist = round(float(val['close_approach_data'][0]['miss_distance']['kilometers']), 3)
 							else:
 								tmp_ast_miss_dist = -1
 						else:
+							#--- piešķir neiespejamos datus lai varētu tos atšķirt
 							tmp_ast_close_appr_ts = -1
 							tmp_ast_close_appr_dt_utc = "1969-12-31 23:59:59"
 							tmp_ast_close_appr_dt = "1969-12-31 23:59:59"
@@ -83,11 +101,14 @@ if r.status_code == 200:
 						tmp_ast_speed = -1
 						tmp_ast_miss_dist = -1
 
+
+					#--- izprintē asteroidu info.
 					print("------------------------------------------------------- >>")
 					print("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
 					print("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
 					print("Speed: " + str(tmp_ast_speed) + " km/h" + " | MISS distance: " + str(tmp_ast_miss_dist) + " km")
 					
+					#--- datus ieliek attiecīgā sarakstā balstoties uz tmp_ast_hazardous
 					# Adding asteroid data to the corresponding array
 					if tmp_ast_hazardous == True:
 						ast_hazardous.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
@@ -99,10 +120,11 @@ if r.status_code == 200:
 
 	print("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
 
+	#--- noskaidro vai ir kāds objekts par tuvu zemei
 	if len(ast_hazardous) > 0:
 
 		ast_hazardous.sort(key = lambda x: x[4], reverse=False)
-
+		#--- izprintē info par asterōīdiem kas ir par tuvu.
 		print("Today's possible apocalypse (asteroid impact on earth) times:")
 		for asteroid in ast_hazardous:
 			print(str(asteroid[6]) + " " + str(asteroid[0]) + " " + " | more info: " + str(asteroid[1]))
